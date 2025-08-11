b29ad281416c8df1ca35ac7c8390417478804a94
Vu Tran <nguyenvutran2601@gmail.com>
2025-08-11 23:30:31 +0700

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
       <body>
-          {children}
+        {children}
       </body>
     </html>
   );
